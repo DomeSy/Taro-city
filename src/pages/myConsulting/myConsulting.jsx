fa8c692759698a52c3d1612466a99d01eef64d33
Domesy <397163123@qq.com>
2020-10-18 22:44:14 +0800

    原型绘制

--- src/pages/myConsulting/myConsulting.jsx
+++ src/pages/myConsulting/myConsulting.jsx
@@ -2,18 +2,58 @@ import React, { Component } from 'react'
 import { View } from '@tarojs/components';
 import './myConsulting.scss'
 
-class Card extends Component {
+class Index extends Component {
   constructor(){
     super(...arguments)
+    this.state={
+      list: [
+        {
+          text: '申报号：',
+          value: '130101000407458'
+        },
+        {
+          text: '事项名称：',
+          value: '130101000407458'
+        },
+        {
+          text: '申报名称：',
+          value: '130101000407458'
+        },
+        {
+          text: '申报名称：',
+          value: '130101000407458'
+        },
+        {
+          text: '申报名称：',
+          value: '130101000407458'
+        },
+        {
+          text: '办理状态：',
+          value: '我哦哦哦哦哦哦哦我哦哦哦哦哦哦哦我哦哦哦哦哦哦哦我哦哦哦哦哦哦哦'
+        }
+      ]
+    }
   }
+  
   render() {
 
+    const { list } = this.state
+
     return (
-      <View>
-        咨询详情
+      <View className="MyConsulting">
+        <View className="MyConsulting-table">
+          {
+            list.map((item, index) => (
+              <View className="MyConsulting-table-list" key={index}>
+                <View className="MyConsulting-table-list-text">{item.text}</View>
+                <View className="MyConsulting-table-list-value">{item.value}</View>
+              </View>
+            ))
+          }
+        </View>
       </View>
     )
   }
 }
 
-export default Card
+export default Index
