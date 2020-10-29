a9714e55a27da3efc86be5626c76f39d6ea1e3f1
Domesy <397163123@qq.com>
2020-10-29 09:37:12 +0800

    处理返回数据

--- src/reducers/home.js
+++ src/reducers/home.js
@@ -7,13 +7,34 @@ const INITIAL_STATE = {
   }
 }
 
+function JamsData(cols) {
+  let homeList = {};
+  for(let i = 0; i < cols.length; i++) {
+    if(cols[i].resourcename === '热门应用') {
+      homeList.hotLiest = cols[i]
+    } else if(cols[i].resourcename === '主题专区') {
+      homeList.themeList = cols[i]
+    } else if(cols[i].resourcename === '主题服务') {
+      homeList.ThemeServiceList = cols[i]
+    } else if(cols[i].resourcename === '上新') {
+      homeList.newList = cols[i]
+    } else if(cols[i].resourcename === '图片新闻') {
+      homeList.rationList = cols[i]
+    } else if(cols[i].resourcename === '我的证照') {
+      homeList.licenceList = cols[i]
+    } 
+  }
+  return homeList
+}
+
 export default function counter (state = INITIAL_STATE, action) {
   switch (action.type) {
     case HOMEINIT: {
-      console.log(action, '00098')
+      const { cols } = action.payload;
+      const homeList = JamsData(cols)
       return {
         home: {
-          ...action.payload
+          ...homeList
         }
       }
     }
