a21386f077c21caeabf3c909820d1de75a18c948
Domesy <397163123@qq.com>
2020-11-10 10:23:27 +0800

    删除无用图片

--- src/pages/opinion/opinion.jsx
+++ src/pages/opinion/opinion.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { View } from '@tarojs/components';
-import { Textarea, ImagePicker, Button } from '@components';
-import { JmasRequest, Method, Jump } from '@unilts'
+import { View } from '@tarojs/components'
+import Taro from '@tarojs/taro'
+import { Textarea, ImagePicker, Button } from '@components'
+import { AtMessage } from 'taro-ui'
 
 import './opinion.scss'
 
@@ -23,19 +24,23 @@ class Opinion extends Component {
   }
 
   onClick = () => {
-    Jump({url: '/index', method: 'switchTab'})
-
+    Taro.atMessage({
+      message: `服务正在建设中尽情期待`,
+      type: 'error',
+      duration: 2000
+    }) 
   }
 
   render() {
 
     return (
       <View className="Opinion">
+        <AtMessage />
         <Textarea maxLength="140" placeholder="请在此输入您的反馈内容，以便我们更好的为您服务， 谢谢！" onChange={this.onChangeText}/>
         <ImagePicker count={4} allCount={4} onChange={(file) => this.onChangeImage(file) }/>
         <View className="Opinion-border" />
-        <Button></Button>
-        <View className="Opinion-tip" onClick={() => this.onClick()}>联系我们</View>
+        <Button onClick={()=>this.onClick()}></Button>
+        <View className="Opinion-tip">联系我们</View>
         <View className="Opinion-tel">0531-81286501 / 0531-81286501</View>
       </View>
     )
