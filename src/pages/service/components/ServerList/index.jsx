be4b6c679e8a6b2a13dce26db0a2071c092998c0
Domesy <397163123@qq.com>
2020-11-19 11:12:46 +0800

    更改原型

--- src/pages/service/components/ServerList/index.jsx
+++ src/pages/service/components/ServerList/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { View } from '@tarojs/components'
-import { Title, ListTheme, TitleBig } from '@components'
+import { Title, ListTheme } from '@components'
 import { connect } from 'react-redux'
 import { Method } from '@unilts'
 
@@ -10,43 +10,18 @@ import './index.scss'
 class Index extends Component {
   constructor(){
     super(...arguments)
-    this.state = {
-      titleList: [
-        {
-          text: '个人服务',
-          value: 0,
-          isUnder: true
-        },
-        {
-          text: '法人服务',
-          value: 1,
-          isUnder: false
-        }
-      ],
-      value: 0
-    }
-  }
-
-  onChang = ( value ) => {
-    this.setState({
-      value
-    })
   }
 
   render() {
-    const { titleList, value } = this.state;
     let list = [];
-
     if(!Method.isObject(this.props.home)){
-      let {  personList, legalList } = this.props.home;
-      personList = personList.listAll.length === 0 ? personList : personList.listAll;
-      legalList = legalList.listAll.length === 0 ? legalList : legalList.listAll;
-      list = value === 0 ? personList : legalList;
+      let {  themeserveList } = this.props.home;
+      list = themeserveList.listAll.length === 0 ? themeserveList : themeserveList.listAll;
     }
 
     return (
       <View className="ServerList">
-        <TitleBig list={titleList} onChang={this.onChang}/>
+        <Title title="主题服务" none/>
         <ListTheme list={list}/>
       </View>
     );
