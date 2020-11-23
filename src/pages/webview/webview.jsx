013d1c8db9d28b2308c7b701d2ed20fd3ba60514
Domesy <397163123@qq.com>
2020-11-23 09:46:58 +0800

    修改webVIew

--- src/pages/webview/webview.jsx
+++ src/pages/webview/webview.jsx
@@ -2,12 +2,10 @@ import React, { Component } from 'react'
 import Taro, { getCurrentInstance } from '@tarojs/taro'
 import { View, WebView } from '@tarojs/components'
 import './webview.scss'
+import { connect } from 'react-redux'
 
+@connect(({user}) => user)
 class Webview extends Component {
-
-  config = {
-    navigationBarTitleText: ''
-  }
 
   constructor(){
     super(...arguments)
@@ -18,18 +16,22 @@ class Webview extends Component {
 
   componentDidShow(){
     const { title = '', url } = getCurrentInstance().router.params;
+    console.log(url, '000')
     this.setState({
       url
     })
     Taro.setNavigationBarTitle({ title })
   }
 
+  onFaceAlipay = (e) => {
+    console.log(e, '---')
+  }
 
   render() {
     const { url } = this.state
     return (
       <View className="Webview">
-       <WebView src={url}  />
+        <WebView id="onFaceAlipay" src={url} onMessage={(e) => onFaceAlipay(e) } />
       </View>
     )
   }
